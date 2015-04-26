54cdf663ed6a5752824bb50aad7b9ee9a21335ea
nirasan <niranirao@gmail.com>
2015-04-26 15:32:24 +0900

    set routes to root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :questions
   devise_for :users
+  
+  resources :questions
+
+  root to: "questions#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

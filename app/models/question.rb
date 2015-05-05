66816a9eae97151c1cdc6670e91cefc48c38216e
nirasan <niranirao@gmail.com>
2015-05-06 01:14:40 +0900

    add validates

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,6 +6,8 @@ class Question < ActiveRecord::Base
   enumerize :category, in: %i(man animal landscape illustration other), default: :man
 
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
-
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+  validates :title, :presence => true
+  validates :image, :presence => true
 end

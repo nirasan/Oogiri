1c7243f10fe1641383961195e123c8a785191dd9
nirasan <niranirao@gmail.com>
2015-05-06 00:08:05 +0900

    delete comment

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,28 +2,20 @@ class QuestionsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_question, only: [:show, :edit, :update, :destroy]
 
-  # GET /questions
-  # GET /questions.json
   def index
     @questions = current_user.questions
   end
 
-  # GET /questions/1
-  # GET /questions/1.json
   def show
   end
 
-  # GET /questions/new
   def new
     @question = current_user.questions.build
   end
 
-  # GET /questions/1/edit
   def edit
   end
 
-  # POST /questions
-  # POST /questions.json
   def create
     @question = current_user.questions.build(question_params)
 
@@ -38,8 +30,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /questions/1
-  # PATCH/PUT /questions/1.json
   def update
     respond_to do |format|
       if @question.update(question_params)
@@ -52,8 +42,6 @@ class QuestionsController < ApplicationController
     end
   end
 
-  # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
     @question.destroy
     respond_to do |format|
@@ -63,12 +51,10 @@ class QuestionsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_question
       @question = current_user.questions.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def question_params
       params.require(:question).permit(:title, :category, :image)
     end

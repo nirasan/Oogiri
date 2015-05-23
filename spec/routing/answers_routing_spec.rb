69c22e5ec9d1ac406e9bb061e9713f9e31897ad3
nirasan <niranirao@gmail.com>
2015-05-23 19:48:18 +0900

    Answerのルーティングテスト修正

--- spec/routing/answers_routing_spec.rb
+++ spec/routing/answers_routing_spec.rb
@@ -4,31 +4,31 @@ RSpec.describe AnswersController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/answers").to route_to("answers#index")
+      expect(:get => "/questions/1/answers").to route_to(:"answers#index", :question_id => "1")
     end
 
     it "routes to #new" do
-      expect(:get => "/answers/new").to route_to("answers#new")
+      expect(:get => "/questions/1/answers/new").to route_to("answers#new", :question_id => "1")
     end
 
     it "routes to #show" do
-      expect(:get => "/answers/1").to route_to("answers#show", :id => "1")
+      expect(:get => "/questions/1/answers/1").to route_to("answers#show", :question_id => "1", :id => "1")
     end
 
     it "routes to #edit" do
-      expect(:get => "/answers/1/edit").to route_to("answers#edit", :id => "1")
+      expect(:get => "/questions/1/answers/1/edit").to route_to("answers#edit", :question_id => "1", :id => "1")
     end
 
     it "routes to #create" do
-      expect(:post => "/answers").to route_to("answers#create")
+      expect(:post => "/questions/1/answers").to route_to("answers#create", :question_id => "1")
     end
 
     it "routes to #update" do
-      expect(:put => "/answers/1").to route_to("answers#update", :id => "1")
+      expect(:put => "/questions/1/answers/1").to route_to("answers#update", :question_id => "1", :id => "1")
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/answers/1").to route_to("answers#destroy", :id => "1")
+      expect(:delete => "/questions/1/answers/1").to route_to("answers#destroy", :question_id => "1", :id => "1")
     end
 
   end

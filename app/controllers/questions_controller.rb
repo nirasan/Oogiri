707835ba2fff8b3c4426d8f888a936a1be4fe0ff
nirasan <niranirao@gmail.com>
2015-05-24 22:40:58 +0900

    question と answer の部分テンプレート作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
   before_action :set_question, only: [:edit, :update, :destroy]
 
   def index
-    @questions = Question.all.page(params[:page]).per(3)
+    @questions = Question.all.page(params[:page])
   end
 
   def show

99f030cb57d527998d0384c16bbff42740e22dde
千葉大二郎 <chibadaijirou@chibadaijirou-no-MacBook-Pro.local>
2015-06-24 12:13:55 +0900

    不要になったメソッドの削除

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,21 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :answer
-
-  def self.answer_ranking
-    ranking_data = Comment.group(:answer).sum(:rate)
-    rank = 0
-    before = -1
-    ranking_data
-      .keys
-      .sort{|a, b| ranking_data[b] <=> ranking_data[a]}
-      .map{|answer|
-        rate = ranking_data[answer]
-        if before != rate then
-          before = rate
-          rank += 1
-        end
-        {answer: answer, rank: rank, rate: rate}
-      }
-  end
 end

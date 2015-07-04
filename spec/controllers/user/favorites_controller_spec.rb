b46e01780ff582885177d0843890774294cfe165
千葉大二郎 <chibadaijirou@chibadaijirou-no-MacBook-Pro.local>
2015-07-04 17:19:07 +0900

    bundle exec rspec が落ちないようにだけ修正

--- spec/controllers/user/favorites_controller_spec.rb
+++ spec/controllers/user/favorites_controller_spec.rb
@@ -18,7 +18,7 @@ require 'rails_helper'
 # Message expectations are only used when there is no simpler way to specify
 # that an instance is receiving a specific message.
 
-RSpec.describe FavoritesController, type: :controller do
+RSpec.describe User::FavoritesController, type: :controller do
 
   # This should return the minimal set of attributes required to create a valid
   # Favorite. As you add validations to Favorite, be sure to
--- spec/routing/comments_routing_spec.rb
+++ spec/routing/comments_routing_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe CommentsController, type: :routing do
+RSpec.describe User::CommentsController, type: :routing do
   describe "routing" do
 
     it "routes to #index" do

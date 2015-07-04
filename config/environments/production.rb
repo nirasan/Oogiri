def087374099541be1689d36502872ccbdbc2e33
千葉大二郎 <chibadaijirou@chibadaijirou-no-MacBook-Pro.local>
2015-07-04 13:29:21 +0900

    use mandrill addon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default charset: 'utf-8'
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.mandrillapp.com',
+    port: 587,
+    authentication: "plain",
+    domain: "gentle-ravine-7541.herokuapp.com",
+    enable_starttls_auto: true,
+    user_name: ENV['MANDRILL_USERNAME'],
+    password: ENV['MANDRILL_API_KEY']
+  }
 end

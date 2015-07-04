0895d4d96faf68c862621ccf4fa5414a9a36ab60
千葉大二郎 <chibadaijirou@chibadaijirou-no-MacBook-Pro.local>
2015-07-04 17:31:30 +0900

    views のテストを answers/index だけ試してみて、あとは削除

--- spec/views/answers/index.html.haml_spec.rb
+++ spec/views/answers/index.html.haml_spec.rb
@@ -1,28 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "answers/index", type: :view do
-  before(:each) do
-    assign(:answers, [
-      Answer.create!(
-        :user => nil,
-        :question => nil,
-        :body => "MyText",
-        :category => "Category"
-      ),
-      Answer.create!(
-        :user => nil,
-        :question => nil,
-        :body => "MyText",
-        :category => "Category"
-      )
-    ])
+  let!(:user1) { create(:user) }
+  let!(:question) { create(:question, user: user1) }
+  let!(:answer1) { create(:answer, user: user1, question: question, body: "answer") }
+  let!(:answer2) { create(:answer, user: user1, question: question, body: "answer") }
+
+  before do
+    assign(:answers, Answer.all.page(1));
   end
 
   it "renders a list of answers" do
+    allow(view).to receive(:current_user) { user1 }
+    allow(view).to receive(:user_signed_in?) { false }
     render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
-    assert_select "tr>td", :text => "Category".to_s, :count => 2
+    assert_select ".answer-panel > p.text-center.lead", :text => "answer", :count => 2
   end
 end
